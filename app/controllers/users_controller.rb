f5b57473d37f2a0db13b4e84a611f0d55dfaa5a6
Bookfan97 <sheltielover432@gmail.com>
2019-09-30 20:23:30 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+	@user = User.find(params[:id])
   end
+  def new
+	@user = User.new
+  end
+def create
+@user = User.new(params[:user])
+if @user.save
+            flash[:success] = "Welcome to the Sample App!"
+else
+render 'new'
+end
+end
+private
+def user_params
+params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,7 +30,7 @@ test "email validation should accept valid addresses" do
   end
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-duplicate_user.email = @user.email.upcase
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
